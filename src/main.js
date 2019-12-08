de799ce14c5af6996806f2356231711a7333669f
Noah Horwitz <00noah@gmail.com>
2019-12-07 16:15:45 -0800

    update to template w node true and other corrections.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import { Xyz } from './backend.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+
 $(document).ready(function(){
+  
   console.log("hello");
 
 });
